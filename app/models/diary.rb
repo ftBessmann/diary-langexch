b4ba27fccc50b41fdddf1bc20c987fd990e330fa
nbkhope <ryumemaru@yahoo.com>
2016-05-13 21:05:30 -0700

    Add models for Diary and DiaryEntry

--- app/models/diary.rb
+++ app/models/diary.rb
@@ -2,4 +2,6 @@ class Diary < ActiveRecord::Base
 	# A diary belongs to a profile, but the database table diaries
 	# has owner_id instead of profile_id
   belongs_to :owner, class_name: "Profile"
+
+  has_many :diary_entries
 end
