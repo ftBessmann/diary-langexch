64d6aad4302389d97eebdd9c5911ce71f4311e9b
nbkhope <ryumemaru@yahoo.com>
2016-05-13 22:38:07 -0700

    Rename diary owner to profile

--- db/migrate/20160513205557_create_diaries.rb
+++ db/migrate/20160513205557_create_diaries.rb
@@ -1,7 +1,7 @@
 class CreateDiaries < ActiveRecord::Migration
   def change
   	create_table :diaries do |t|
-  		t.belongs_to :owner, index: true
+  		t.belongs_to :profile, index: true
 
   		t.timestamps null: false
   	end
