c8f5dd8bae41c6ef8feae5b8578b097dcf16564d
nbkhope <ryumemaru@yahoo.com>
2016-05-13 21:01:50 -0700

    Add association between Profile and diaries

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,3 +1,6 @@
 class Profile < ActiveRecord::Base
   belongs_to :user
+
+  # This could be extended to many diaries
+  has_one :diaries
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ActiveRecord::Base
+	# This could be extended to many profiles
   has_one :profile
 end
