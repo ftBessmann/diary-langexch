56512b8efca62bf8cf30806a631ed9d73822ce42
nbkhope <ryumemaru@yahoo.com>
2016-05-13 22:36:01 -0700

    Add belongs_to :language to DiaryEntry model and add helper method to get diary entry's author (profile)

--- app/models/diary.rb
+++ app/models/diary.rb
@@ -1,7 +1,5 @@
 class Diary < ActiveRecord::Base
-	# A diary belongs to a profile, but the database table diaries
-	# has owner_id instead of profile_id
-  belongs_to :owner, class_name: "Profile"
+  belongs_to :profile
 
   has_many :diary_entries
 end
