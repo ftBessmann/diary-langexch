a51f028990a2c36555fe6376c3a5d87d504c25a6
nbkhope <ryumemaru@yahoo.com>
2016-05-14 19:25:10 -0700

    Add controller routes for diaries along with diaries/show view

--- app/controllers/diaries.rb
+++ app/controllers/diaries.rb
@@ -4,6 +4,17 @@ get '/diaries' do
 	erb :'diaries/index'
 end
 
+# Display diary entries for a specific diary
+get '/diaries/:id/diary_entries' do
+	@diary_entries = DiaryEntry.where(diary_id: params[:id])
+
+	erb :'diary_entries/index'
+end
+
+get '/diaries/:id/diary_entries/new' do
+	erb :'diary_entries/new'
+end
+
 # Display a specific diary entry from somebody's diary
 get '/diaries/:diary_id/diary_entries/:id' do
 	@diary_entry = DiaryEntry.find_by(id: params[:id])
