288976aef92a9f1a9eb341f05f799314a782c8ef
nbkhope <ryumemaru@yahoo.com>
2016-05-14 19:27:09 -0700

    Add route for Languages and Users controllers

--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -4,6 +4,13 @@ get '/users' do
 	erb :'users/index'
 end
 
+# Same as get /users/new
+get '/register' do
+	@user = User.new
+
+	erb :'users/new'
+end
+
 get '/users/:id' do
 	@user = User.find_by(id: params[:id])
 
