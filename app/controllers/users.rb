349913983f1c9a40e244778795b8ed80053924d0
Renan Martins & Shaun Sweet <pair+nbkhope.shaun-sweet@devbootcamp.com>
2016-05-16 12:37:14 -0700

    Add registration form and modify routes in controller

--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -7,12 +7,26 @@ end
 # Same as get /users/new
 get '/register' do
 	@user = User.new
-
+  @countries = Country.all
+  @languages = Language.all
 	erb :'users/new'
+end
+
+post '/register' do
+  @user = User.new(params[:user])
+  @profile = Profile.new(params[:profile])
+  @profile.native_language = NativeLanguage.new(language_id: params[:native_language_id])
+  @profile.foreign_language = ForeignLanguage.new(language_id: params[:foreign_language_id])
+  if @user.save
+    login(@user)
+    redirect "/"
+  else
+    erb :'/users/new'
+  end
 end
 
 get '/users/:id' do
 	@user = User.find_by(id: params[:id])
 
-	erb :'users/show'	
+	erb :'users/show'
 end
