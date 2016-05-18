d8854a2fc3ac4e1cee499d2895c1da7756d448ff
nbkhope <ryumemaru@yahoo.com>
2016-05-17 21:45:11 -0700

    Add null: false constraints to users and profiles tables

--- db/migrate/20160513200822_create_users.rb
+++ db/migrate/20160513200822_create_users.rb
@@ -1,11 +1,13 @@
 class CreateUsers < ActiveRecord::Migration
   def change
   	create_table :users do |t|
-  		t.string :username
+  		t.string :username, null: false
   		t.string :password_digest
-  		t.string :email
+  		t.string :email, null: false
 
   		t.timestamps null: false
   	end
+
+  	add_index :users, :email, unique: true
   end
 end
